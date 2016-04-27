50ff7b3f98105e3e21682afb2730b70ea3d3f371
Alex Forey <me@alexforey.com>
2016-04-27 14:01:30 +0100

    Finished front page

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Grey Matter | Edinburgh Fringe</title>
+    <meta name="author" content="Alex Forey">
+    <meta name="title" content="Grey Matter | Edinburgh Fringe">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+
+    <nav>
+      <ul>
+        <li><a href="#play">The Play</a></li>
+        <li><a href="team">Development</a></li>
+        <li class="title">Grey Matter</li>
+        <li><a href="http://diary.greymatterplay.com" class="blog"><span>Production Diary</span></a></li>
+        <li><a href="tickets" class="book">Tickets</a></li>
+      </ul>
+    </nav>
+
+    <section class="bigbox fullheight">
+      <header>
+        <h1>Grey Matter</h1>
+        <h2>A new play</h2>
+
+        <a href="tickets" class="book big">Book Tickets</a>
+      </header>
+    </section>
+
+    <section>
+      <div class="container">
+
+        <aside class="box map">
+          <h2>Performances</h2>
+          <p class="dates">1:15pm 21<sup>st</sup> to 29<sup>th</sup> August</p>
+          <p class="venue">C Nova Studio 3, India Buildings,<br>Victoria Street, EH1 2EX</p>
+          <p><a href="/tickets" class="tickets">Tickets &pound;11-15</a></p>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2234.0481259650846!2d-3.1951715483971097!3d55.94853778051094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4887c79ac3fcf597%3A0xf5778ee5337f5a2f!2sIndia+Buildings%2C+Victoria+St%2C+Edinburgh+EH1+2EX!5e0!3m2!1sen!2suk!4v1461756000325" frameborder="0" style="border:0" ></iframe>
+        </aside>
+
+        <div class="box">
+          <h1>Info about stuff</h1>
+          <p>Some nice blurb about the play. There will be plenty of words here to make sure that it fills out the container. It will be really interesting to the audience.</p>
+          <p>Here's another short paragraph. This one has <a href="team">a link</a> in it.</p>
+
+          <h2 id="play">Some small background</h2>
+          <p>Here's some more text about things that you should read about. It's super interesting and you should read it.</p>
+        </div>
+
+      </div>
+    </section>
+
+    <footer class="container">
+      <div class="contact">
+        <script type="text/javascript">
+        //<![CDATA[
+        <!--
+        var x="function f(x){var i,o=\"\",l=x.length;for(i=0;i<l;i+=2) {if(i+1<l)o+=" +
+        "x.charAt(i+1);try{o+=x.charAt(i);}catch(e){}}return o;}f(\"ufcnitnof x({)av" +
+        " r,i=o\\\"\\\"o,=l.xelgnhtl,o=;lhwli(e.xhcraoCedtAl(1/)3=!88{)rt{y+xx=l;=+;" +
+        "lc}tahce({)}}of(r=i-l;1>i0=i;--{)+ox=c.ahAr(t)i};erutnro s.buts(r,0lo;)f}\\" +
+        "\"(1)11\\\\,j\\\"t`ng)txib|[bKL30\\\\0X\\\\Q[IC05\\\\0[\\\\CCMC>w%Ffu0aai4c" +
+        "&8\\\\^\\\\\\\"!\\\\4F03\\\\\\\\33\\\\01\\\\00\\\\\\\\32\\\\05\\\\00\\\\\\\\"+
+        "OO4700\\\\\\\\10\\\\06\\\\00\\\\\\\\0M03\\\\\\\\1>++!20'\\\"\\\\\\\\\\\\.4 " +
+        ";05\\\\07\\\\\\\"\\\\\\\\\\\\I^VU7T01\\\\\\\\\\\\\\\\\\\\\\\\CEJD5D00\\\\\\" +
+        "\\6y03\\\\\\\\zGsov91)a9nan}twn/\\\\\\\\22\\\\07\\\\03\\\\\\\\25\\\\03\\\\0" +
+        "0\\\\\\\\27\\\\05\\\\03\\\\\\\\24\\\\0\\\\\\\\(\\\"}fo;n uret}r);+)y+^(i)t(" +
+        "eAodrCha.c(xdeCoarChomfrg.intr=So+7;12%=;y++)y11<1(iif){++;i<l;i=0(ior;fthn" +
+        "glex.l=\\\\,\\\\\\\"=\\\",o iar{vy)x,f(n ioctun\\\"f)\")"                    ;
+        while(x=eval(x));
+        //-->
+        //]]>
+        </script>
+        <a href="https://twitter.com/greymatterplay"><img src="images/twitter.svg"></a>
+        <a href="https://facebook.com/greymatterplay"><img src="images/facebook.svg"></a>
+        <a href="https://instragram.com/greymatterplay"><img src="images/instagram.svg"></a>
+        <a href="http://diary.greymatterplay.com"><img src="images/tumblr.svg"></a>
+      </div>
+      <p>&copy; Spasm 2016. Designed by <a href="https://alexforey.com">Alex Forey</a></p>
+    </footer>
+
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  </body>
+
+</html>
