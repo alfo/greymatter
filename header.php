a90d99be91fd2c7ba19e1fca2c661bae4b9e19f1
Alex Forey <me@alexforey.com>
2016-05-03 22:46:26 +0100

    Rename images to photos

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <nav id="greymatter">
       <ul>
         <li><a href="/origins.php">Origins</a></li>
-        <li><a href="/images.php">Images</a></li>
+        <li><a href="/photos.php">Photos</a></li>
         <li class="title"><a href="/">Grey Matter</a></li>
         <li><a href="http://diary.greymatterplay.com" class="blog"><span>Production Diary</span></a></li>
         <li><a href="/tickets" class="book">Tickets</a></li>
