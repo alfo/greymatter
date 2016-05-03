c1c58ecb2cafa3027fb35a7d85f63480b650cd6d
Alex Forey <me@alexforey.com>
2016-05-03 22:31:00 +0100

    Added images page and finished Tumblr

---
+++ images.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+    <section class="bigbox">
+    </section>
+
+    <section>
+      <div class="container">
+
+        <div class="box">
+          <h1>Images</h1>
+
+          <p>From the preview production, taken by Alex Forey:</p>
+
+          <img src="images/greymatter-03.jpg" class="full-width">
+          <img src="images/greymatter-05.jpg" class="full-width">
+          <img src="images/greymatter-10.jpg" class="full-width">
+          <img src="images/greymatter-11.jpg" class="full-width">
+          <img src="images/greymatter-22.jpg" class="full-width">
+
+        </div>
+
+      </div>
+
+    </section>
+<?php include 'footer.php'; ?>
